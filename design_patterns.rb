962f5b4daf1002c7541ea5b14a38f2d9eb8de06a
Zachary Belzer <zbelzer@gmail.com>
2013-12-22 12:32:15 -0600

    Update formatting, add placeholders

--- design_patterns.rb
+++ design_patterns.rb
@@ -1,112 +1,225 @@
 # Creational Patterns
+# =============================================================================
 
-# Abstract Factory: Provide an interface for creating families of related or
-# dependent objects without specifying their concrete classes.
-
-
-# Builder: Separate the construction of a complex object from its
-# representation so that the same construction process can create different
-# representations.
-
-
-# Factory Method: Define an interface for creating an object, but let
-# subclasses decide which class to instantiate. +Factory Method+ lets a class
-# defer instantiation to subclasses.
-
-# Prototype: Specify the kinds of objects to create using a prototypical
-# instance, and creating new objects by copying this prototype.
+# Abstract Factory:
+# Provide an interface for creating families of related or dependent objects
+# without specifying their concrete classes.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Singleton: Ensure a class only has one instance and provide a global point of
-# access to it.
+# Builder:
+# Separate the construction of a complex object from its representation so that
+# the same construction process can create different representations.
+#
+# Example in Ruby:
+#
+# In the wild:
+
+
+# Factory Method:
+# Define an interface for creating an object, but let subclasses decide which
+# class to instantiate. +Factory Method+ lets a class defer instantiation to
+# subclasses.
+#
+# Example in Ruby:
+#
+# In the wild:
+
+
+# Prototype:
+# Specify the kinds of objects to create using a prototypical instance, and
+# creating new objects by copying this prototype.
+#
+# Example in Ruby:
+#
+# In the wild:
+
+
+# Singleton:
+# Ensure a class only has one instance and provide a global point of access to
+# it.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
 # Structural Patterns
+# =============================================================================
 
-# Adapter: Convert the interface of a class into another interface clients
-# expect.  Adapter lets classes work together that couldn't otherwise because
-# of incompatible interfaces.
-
-
-# Bridge: Decouple an abstraction from its implementation so that the two can
-# vary independently.
-
-
-# Composite: Compose objects into tree structures to represent part-whole
-# hierarchies.  +Composite+ lets clients treat individual objects and
-# compositions of objects uniformly.
+# Adapter:
+# Convert the interface of a class into another interface clients expect.
+# Adapter lets classes work together that couldn't otherwise because of
+# incompatible interfaces.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Decorator: Attach additional responsibilities to an object dynamically.
-# Decorators provide a flexible alternative to subclassing for extending
-# functionality.
+# Bridge:
+# Decouple an abstraction from its implementation so that the two can vary
+# independently.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Facade: Provide a unified interface to a set of interfaces in a subsystem.
-# Facade defines a higher level interface that makes the subsystem easier to
-# use.
+# Composite:
+# Compose objects into tree structures to represent part-whole hierarchies.
+# +Composite+ lets clients treat individual objects and compositions of objects
+# uniformly.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Flyweight: Use sharing to support large numbers of fine-grained objects
-# efficiently.
+# Decorator:
+# Attach additional responsibilities to an object dynamically.  Decorators
+# provide a flexible alternative to subclassing for extending functionality.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Proxy: Provide a surrogate or placeholder for another object to control
-# access to it.
+# Facade:
+# Provide a unified interface to a set of interfaces in a subsystem.  +Facade+
+# defines a higher level interface that makes the subsystem easier to use.
+#
+# Example in Ruby:
+#
+# In the wild:
+
+
+# Flyweight:
+# Use sharing to support large numbers of fine-grained objects efficiently.
+#
+# Example in Ruby:
+#
+# In the wild:
+
+
+# Proxy:
+# Provide a surrogate or placeholder for another object to control access to
+# it.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
 # Behavioral Patterns:
+# =============================================================================
 
-# Chain of Responsibility: Avoid coupling the sender of a request to its
-# receiver by giving more than one chance to handle the request. Chain the
-# receiving objects and pass the request along the chain until an object
-# handles it.
+# Chain of Responsibility:
+# Avoid coupling the sender of a request to its receiver by giving more than
+# one chance to handle the request. Chain the receiving objects and pass the
+# request along the chain until an object handles it.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Command: Encapsulate a request as an object, thereby letting you parameterize
-# clients with different requests, queue or log requests, and support undoable
+# Command:
+# Encapsulate a request as an object, thereby letting you parameterize clients
+# with different requests, queue or log requests, and support undoable
 # operations.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Interpreter: Given a language, define a representation for its grammar along
-# with an interpreter that uses the representation to interpret sentences in
-# the language.
+# Interpreter:
+# Given a language, define a representation for its grammar along with an
+# interpreter that uses the representation to interpret sentences in the
+# language.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Iterator: Provide a way to access the elements of an aggregate object
-# sequentially without exposing the underlying representation.
-
-# @example: Tree level-order
-
-
-# Mediator: Define an object that encapsulates how a set of objects interact.
-# Mediator promotes loose coupling by keeping objects from referring to each
-# other explicitly, and it lets you vary their interaction independently.
-
-
-# Memento: Without violating encapsulation, capture and externalize an objects
-# internal state so that the object can be restored to this state later.
+# Iterator:
+# Provide a way to access the elements of an aggregate object sequentially
+# without exposing the underlying representation.
+#
+# Example in Ruby:
+#
+# In the wild:
+# Tree level-order
 
 
-# Observer: Define a one-to-many dependency between objects so that when one
-# object changes state, all its dependents are notified and updated
-# automatically.
+# Mediator:
+# Define an object that encapsulates how a set of objects interact.  Mediator
+# promotes loose coupling by keeping objects from referring to each other
+# explicitly, and it lets you vary their interaction independently.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# State: Allow an object to alter its behavior when its internal state changes.
-# The object will appear to change its class.
+# Memento:
+# Without violating encapsulation, capture and externalize an objects internal
+# state so that the object can be restored to this state later.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Strategy: Define a family of algorithms, encapsulate each one, and make them
+# Observer:
+# Define a one-to-many dependency between objects so that when one object
+# changes state, all its dependents are notified and updated automatically.
+#
+# Example in Ruby:
+#
+# In the wild:
+
+
+# State:
+# Allow an object to alter its behavior when its internal state changes.  The
+# object will appear to change its class.
+#
+# Example in Ruby:
+#
+# In the wild:
+
+
+# Strategy:
+# Define a family of algorithms, encapsulate each one, and make them
 # interchangeable. Strategy lets the algorithm vary independently from clients
 # that use it.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Template Method: Define the skeleton of an algorithm in an operation,
-# deferring some steps to subclasses. Template Method lets subclasses redefine
-# certain steps of an algorithm without changing the algorithms structure.
+# Template Method:
+# Define the skeleton of an algorithm in an operation, deferring some steps to
+# subclasses. +Template Method+ lets subclasses redefine certain steps of an
+# algorithm without changing the algorithms structure.
+#
+# Example in Ruby:
+#
+# In the wild:
 
 
-# Visitor: Represent an operation to be performed on the elements of an object
-# structure. Visitor lets you define a new operation without changing the
+# Visitor:
+# Represent an operation to be performed on the elements of an object
+# structure. +Visitor+ lets you define a new operation without changing the
 # classes of the elements on which it operates.
+#
+# Example in Ruby:
+#
+# In the wild:
+#
