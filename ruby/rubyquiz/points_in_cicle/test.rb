b27dbe18ebad6d9a99fa5ecf5098f93c9ced954c
Zachary Belzer <zbelzer@gmail.com>
2014-01-04 17:11:18 -0600

    Add drawing for sanity check.

---
+++ ruby/rubyquiz/points_in_cicle/test.rb
@@ -0,0 +1,25 @@
+require_relative 'point_generator'
+
+points    = []
+generator = PointGenerator.new(21)
+
+10000.times do
+  points << generator.generate
+end
+
+svg = <<ENDSVG
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="-100 -100 200 200">
+
+#{points.map{ |point| "<circle cx='#{point.x}' cy='#{point.y}' r='1' />" }.join("\n")}
+
+  <line x1="-5" y1="-20" x2="5" y2="-20" style="stroke:rgb(255,0,0);stroke-width:1" />
+  <line x1="-5" y1="20" x2="5" y2="20" style="stroke:rgb(255,0,0);stroke-width:1" />
+  <line y1="-5" x1="-20" y2="5" x2="-20" style="stroke:rgb(255,0,0);stroke-width:1" />
+  <line y1="-5" x1="20" y2="5" x2="20" style="stroke:rgb(255,0,0);stroke-width:1" />
+
+  <line x1="-100" y1="0" x2="100" y2="0" style="stroke:rgb(255,0,0);stroke-width:1" />
+  <line x1="0" y1="-100" x2="0" y2="100" style="stroke:rgb(255,0,0);stroke-width:1" />
+</svg>
+ENDSVG
+
+File.open('test.svg', 'w') { |f| f.write svg }
