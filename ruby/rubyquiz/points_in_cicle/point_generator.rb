e5fa8950586cb82a03ffb6096b5e650513cf1b8e
Zachary Belzer <zbelzer@gmail.com>
2014-01-04 16:27:00 -0600

    Doc completion

--- ruby/rubyquiz/points_in_cicle/point_generator.rb
+++ ruby/rubyquiz/points_in_cicle/point_generator.rb
@@ -1,10 +1,18 @@
 # From http://rubyquiz.strd6.com/
 
+# Point class to use for convenience.
 Point = Struct.new(:x, :y) do
+  # For pretty printing
+  # @return [String]
   def to_s
     "#<Point(#{x},#{y})>"
   end
 
+  # Create a random point.
+  #
+  # @param [Numeric] x_bound Place x within +/- x_bound
+  # @param [Numeric] y_bound Place y within +/- y_bound
+  # @return [Point]
   def self.random(x_bound, y_bound = nil)
     y_bound ||= x_bound
 
@@ -30,6 +38,7 @@ class PointGenerator
   # Generate a random {Point}
   # @param [Numeric] radius Radius of circle to use
   # @param [Point] origin {Point} in x/y coordinates to place circle
+  # @return [Point]
   def generate
     x = generate_random_x
     y = generate_random_y(x)
@@ -38,14 +47,19 @@ class PointGenerator
   end
 
   # Find a random x to use.
+  # @return [Float]
   def generate_random_x
     max_x = @radius
     (Kernel.rand * 2.0 * max_x) - max_x
   end
+  private :generate_random_x
 
   # Find a random y to use, given a random x.
+  # @param [Numeric] x
+  # @return [Float]
   def generate_random_y(x)
     max_y = Math.sqrt(@radius**2 - x**2)
     (Kernel.rand * 2.0 * max_y) - max_y
   end
+  private :generate_random_y
 end
